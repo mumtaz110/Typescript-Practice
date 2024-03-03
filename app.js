0b7d741e8d27a3432120dcd3b75091786af6049d
mumtaz110 <159236585+mumtaz110@users.noreply.github.com>
2024-03-03 09:21:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var firstName = 'mumtaz';
+var lastname = 'ali';
+var fullname = "".concat(firstName, " ").concat(lastname);
+console.log(fullname);
+var itstudent = true;
---
+++ app.ts
@@ -0,0 +1,8 @@
+let firstName: string = 'mumtaz';
+let lastname: string = 'ali';
+let fullname: string = `${firstName} ${lastname}`;
+
+console.log(fullname);
+
+
+let itstudent: boolean = true;
